5464a0ace0f476e20edf51037661fa2c5a57aef2
ArcShift <ma.kaafi@yahoo.co.id>
2019-10-08 10:44:39 +0700

    fix query login

--- application/models/M_auth.php
+++ application/models/M_auth.php
@@ -15,7 +15,7 @@ class M_auth extends CI_Model {
 
     function login($input) {
         $this->db->select('id, name, username, email, phone_number, mobile_token, bio, url_photo, total_post, following, followers');
-        $this->db->where("email ='" . $input['email'] . "' OR username='" . $input['email'] . "'");
+        $this->db->where("(email ='" . $input['email'] . "' OR username='" . $input['email'] . "')");
         $this->db->where('password', md5($input['password']));
         if ($this->db->count_all_results('fisherman', false) == 1) {
             return $this->db->get()->result_array()[0];
