664ed3f13d7f368b6c180ab51bb89e101abe4343
ArcShift <ma.kaafi@yahoo.co.id>
2019-10-09 11:08:38 +0700

    profil (edit email, ubah password)

--- application/controllers/Profil.php
+++ application/controllers/Profil.php
@@ -1,43 +1,32 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
+require_once(APPPATH . "controllers/BaseAPI.php");
 
-require APPPATH . '/libraries/REST_Controller.php';
-
-class Profil extends REST_Controller {
+class Profil extends BaseAPI {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('m_profil', 'model');
     }
 
-    public function edit_post() {
-        echo 
+    public function edit_post() {//unfinish 
         $input = json_decode(file_get_contents('php://input'), TRUE);
-        $response= array();
+        $response = array();
         print_r($input);
 //        print_r($this->input->post());
     }
+
     public function edit_email_post() {
-        $input = json_decode(file_get_contents('php://input'), TRUE);
-        $response= array();
-        if ($this->model->update_email($input)) {
-            $response['message'] = 'success';
-        } else {
-            $response['message'] = 'failed';
-            $response['error'] = $this->db->error()['message'];
-        }
-        $this->response($response, 200);
+        $input = $this->check_param('id','email');
+        $callback=$this->model->update_email($input);
+        $this->run_query($callback);
     }
+
     public function ubah_password_post() {
-        $input = json_decode(file_get_contents('php://input'), TRUE);
-        $response= array();
-        if ($this->model->update_pass($input)) {
-            $response['message'] = 'success';
-        } else {
-            $response['message'] = 'failed';
-            $response['error'] = $this->db->error()['message'];
-        }
-        $this->response($response, 200);
+        $input = $this->check_param('id','password');
+        $callback=$this->model->update_pass($input);
+        $this->run_query($callback);
     }
+
 }
--- application/models/M_profil.php
+++ application/models/M_profil.php
@@ -6,16 +6,21 @@ class M_profil extends CI_Model {
         $this->db->set('email', $input['email']);
         $this->db->where('id', $input['id']);
         if ($this->db->update('fisherman')) {
-            return true;
+            $this->db->select('id, name, username, email, phone_number, mobile_token, bio, url_photo, total_post, following, followers');
+            $this->db->where('id', $input['id']);
+            return $this->db->get('fisherman')->result_array()[0];
         } else {
             return false;
         }
     }
+
     function update_pass($input) {
         $this->db->set('password', md5($input['password']));
         $this->db->where('id', $input['id']);
         if ($this->db->update('fisherman')) {
-            return true;
+            $this->db->select('id, name, username, email, phone_number, mobile_token, bio, url_photo, total_post, following, followers');
+            $this->db->where('id', $input['id']);
+            return $this->db->get('fisherman')->result_array()[0];
         } else {
             return false;
         }
