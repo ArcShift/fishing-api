45bdb4dd6ea6d268b9f06369563b8ac9262d799f
ArcShift <ma.kaafi@yahoo.co.id>
2019-12-18 23:53:17 +0700

    update field

--- application/models/M_pengumuman.php
+++ application/models/M_pengumuman.php
@@ -3,14 +3,16 @@
 class M_pengumuman extends MY_Model {
 
     function all() {
-        $this->db->select('id, title');
-        return $this->db->get('announcement')->result_array();
+        $this->db->select('a.id, a.title, a.sort_desc, r.nama AS admin, a.created_at');
+        $this->db->join('user u', 'a.user=u.id');
+        $this->db->join('role r', 'u.idUserType=r.id');
+        return $this->db->get('announcement a')->result_array();
     }
 
     function detail($id) {
         $this->db->where('id', $id);
-        $r=$this->db->get('announcement')->row_array();
-        $r['url_img']=base_url('upload/tangkapan/') .$r['url_img'];
+        $r = $this->db->get('announcement')->row_array();
+        $r['url_img'] = base_url('upload/tangkapan/') . $r['url_img'];
         return $r;
     }
 
